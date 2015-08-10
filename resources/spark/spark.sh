b4361b56907e5b047d036c08f01e2af1135b3b63
VIJAYAKUMAR JAWAHARLAL <vijayakumar.jawaharlal@gmail.com>
2015-08-09 20:58:31 -0400

    CLASS PATH environment variables is added to connect mysql hive metastore

--- resources/spark/spark.sh
+++ resources/spark/spark.sh
@@ -1,2 +1,5 @@
 export SPARK_HOME=/usr/local/spark
 export PATH=${SPARK_HOME}/bin:${SPARK_HOME}/sbin:${PATH}
+export CLASSPATH="$CLASSPATH:/usr/local/hive/lib/mysql-connector-java-5.1.26-bin.jar"
+export SPARK_CLASSPATH=$CLASSPATH
+export SPARK_SUBMIT_CLASSPATH=$CLASSPATH
